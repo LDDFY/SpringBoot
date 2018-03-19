eac09f59aea762054a4084031a29ee3ca3112511
changhao <www.changhao@outlook.com>
2018-03-19 21:54:25 +0800

    init project

---
+++ demo/src/main/java/com/demo/HelloController.java
@@ -0,0 +1,40 @@
+package com.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${cupSize}")
+    private String cupSize;
+
+    @Value("${age}")
+    private Integer age;
+
+    @Value("${content}")
+    private String content;
+
+    @Autowired
+    private GirlProperties girlProperties;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String say() {
+        return cupSize + ":" + age;
+    }
+
+    @GetMapping(value = "getContent")
+    public String getContent() {
+        return content;
+    }
+
+    @GetMapping(value = "getGrilProperties")
+    public String getGrilProperties() {
+        return girlProperties.toString();
+    }
+}
+dddddddddd
